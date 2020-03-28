e43e50e938ff6ba99c0e0f1270f473273cb99605
Adam Cross <adam@noneya.business>
2020-03-28 12:32:25 -0500

    update database config

--- src/main/java/dev/adamcross/europa/EuropaApplication.java
+++ src/main/java/dev/adamcross/europa/EuropaApplication.java
@@ -2,10 +2,14 @@ package dev.adamcross.europa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"dev.adamcross.europa.model"})
 public class EuropaApplication {
 
 	public static void main(String[] args) {
